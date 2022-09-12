4a6d334141073d8c0bab3e23b32bdeae82cbeebb
HD\Li <huafeng.li@santech.com.cn>
2022-09-12 23:19:00 +0800

    no message

---
+++ DateSheetChoose/Base/CommandBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DateSheetChoose.Base
+{
+    public class CommandBase : ICommand
+    {
+        private Predicate<object> _canExecute;
+        private Action<object> _execute;
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public CommandBase(Predicate<object> canExecute, Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ DateSheetChoose/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,79 @@
+﻿using DateSheetChoose.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DateSheetChoose.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        private CommandBase _closeComamnd;
+
+        public CommandBase CloseCommand
+        {
+            get
+            {
+                if (_closeComamnd == null)
+                {
+                    _closeComamnd = new CommandBase(
+                        p => true,
+                        p => 
+                        { 
+                            Window window=p as Window;
+                            window.Close();
+                        }
+                        );
+                }
+                return _closeComamnd;
+            }
+        }
+
+        private CommandBase _minComamnd;
+
+        public CommandBase MinCommand
+        {
+            get
+            {
+                if (_minComamnd == null)
+                {
+                    _minComamnd = new CommandBase(
+                        p => true,
+                        p =>
+                        {
+                            Window window = p as Window;
+                            window.WindowState = window.WindowState == WindowState.Minimized ? WindowState.Normal : WindowState.Minimized;
+                        }
+                        );
+                }
+                return _minComamnd;
+            }
+        }
+        private CommandBase _maxComamnd;
+
+        public CommandBase MaxCommand
+        {
+            get
+            {
+                if (_maxComamnd == null)
+                {
+                    _maxComamnd = new CommandBase(
+                        p => true,
+                        p =>
+                        {
+                            Window window = p as Window;
+                            window.WindowState=window.WindowState== WindowState.Maximized? WindowState.Normal: WindowState.Maximized;
+                        }
+                        );
+                }
+                return _maxComamnd;
+            }
+        }
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
