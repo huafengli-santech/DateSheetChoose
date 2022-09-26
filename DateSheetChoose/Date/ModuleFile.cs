c2126b3306e4ee866f5039d1c753c46b1a09e903
LAPTOP-OHJC2HCK\46432 <464325382@qq.com>
2022-09-26 18:19:30 +0800

    no message

--- DateSheetChoose/Date/ModuleFile.cs
+++ DateSheetChoose/Date/ModuleFile.cs
@@ -9,7 +9,9 @@ namespace DateSheetChoose.Date
     public class ModuleFile
     {
         StringBuilder sb = new StringBuilder();
-        public StringBuilder StandardModuleProgram { get
+        public StringBuilder StandardModuleProgram
+        {
+            get
             {
                 sb.Append("$请将文件名命名为模块名称[英文]，比如CMhp\n");
                 sb.Append("$区块名定义为以下方式【冒号使用中文】\n");
@@ -17,6 +19,7 @@ namespace DateSheetChoose.Date
                 sb.Append("$区块名2+空格+中文翻译：参数1+空格+中文解释+空格|+空格参数2+空格+中文解释+空格|...|+空格参数N+空格+中文解释\n");
                 sb.Append("$比如：Type 类型 ：hp 高性能型 | ba 经济型\n");
                 return sb;
-            }  }
+            }
+        }
     }
 }
