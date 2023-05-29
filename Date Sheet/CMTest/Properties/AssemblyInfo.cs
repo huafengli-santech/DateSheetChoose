4bd546368da1161e04ee0bddb3175ea0e3849ed1
huafeng.li <huafeng.li@santech.com.cn>
2023-05-29 18:19:11 +0800

    更改程序集名称

--- Date Sheet/CMTest/Properties/AssemblyInfo.cs
+++ Date Sheet/CMTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("CMTest")]
+[assembly: AssemblyTitle("DataSheet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CMTest")]
+[assembly: AssemblyProduct("DataSheet")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
